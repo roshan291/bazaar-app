c17351d976af8a3a1b926fcf40defcf467eb9c49
roshan291 <roshanhanif8@gmail.com>
2024-10-19 23:57:58 +0530

    global api changes

--- src/API/apiClient.tsx
+++ src/API/apiClient.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'; 
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://webservice-y4qv.onrender.com/';
+// const BASE_URL = 'http://localhost:8000';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
